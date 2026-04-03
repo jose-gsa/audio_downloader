ff65be9b79c0501c23b152038a69d6b8cb107979
jose-gsa <jose_16211@proton.me>
2026-04-02 20:41:55 -0400

    feat: create the main download function

--- src/downloader.py
+++ src/downloader.py
@@ -1,1 +1,20 @@
+import yt_dlp
 
+
+def download_audio(url: str, output_path: str):
+
+    ydl_opts = {
+        "format": "bestaudio/best",  # best audio quality
+        "postprocessors": [
+            {  # FFmpeg config
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+        "outtmpl": f"{output_path}/%(title)s.%(ext)s",  # define the final name
+        "quiet": False,  # Show the progress (debug)
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
